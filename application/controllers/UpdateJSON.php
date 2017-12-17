76bc7fd7bcc45a43025ab29be4791e3f35d5ab26
Gpanayu <gpanayu@yahoo.co.th>
2017-12-17 22:30:17 +0700

    update some files

---
+++ application/controllers/UpdateJSON.php
@@ -0,0 +1,52 @@
+<?php
+class UpdateJSON extends CI_Controller {
+
+public function __construct()
+{
+    parent::__construct();
+    $this->load->helper('file');
+}
+
+public function index()
+{
+    $countries = $this->European_countries_model->get_countries();
+    $data['countries'] = $countries;
+
+    $response = array();
+    $posts = array();
+    foreach ($countries as $country)
+    {
+        $posts[] = array(
+            "title"                 =>  $country->euro_id,
+            "flag"                  =>  $country->flag_name,
+            "population"            =>  $country->population,
+            "avg_annual_gcountryth" =>  $country->avg_annual_gcountryth,
+            "date"                  =>  $country->date
+        );
+    }
+    $response['posts'] = $posts;
+    echo json_encode($response,TRUE);
+
+    //If the json is correct, you can then write the file and load the view
+
+    // $fp = fopen('./eur_countries_array.json', 'w');
+    // fwrite($fp, json_encode($response));
+
+    // if ( ! write_file('./eur_countries_array.json', $arr))
+    // {
+    //     echo 'Unable to write the file';
+    // }
+    // else
+    // {
+    //     echo 'file written';
+    // }
+    // $this->load->view('european_countries_view', $data);
+}
+
+public function urlrequest(){
+
+  //This will get the JSON data from NETPIE server
+  print file_get_contents("https://api.netpie.io/feed/CPCUSoilhumidity?apikey=o9dYEQAyVBqDmWn1SORwfFTq7afHhYjd&granularity=10minutes&since=24hours&filter=humidity");
+}
+
+}?>
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -131,11 +131,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <div class="container">
       <div class="row text-center">
         <div class="col-md-12">
-          <h2 class="heading mt-0 mb-3">Developer</h2>
+          <h2 class="heading mt-0 mb-3">Developers</h2>
           <p class="sub-heading pb_color-light-opacity-8">MEMBER1</p>
           <p class="sub-heading pb_color-light-opacity-8">MEMBER2</p>
           <p class="sub-heading pb_color-light-opacity-8">MEMBER3</p>
-          <p class="sub-heading pb_color-light-opacity-8">MEMBER4</p>
+          <p class="sub-heading pb_color-light-opacity-8">Panayu Keelawat 5831036921</p>
           <p class="sub-heading pb_color-light-opacity-8">Siraphat Gruysiriwong 5831078221</p>
         </div>
 
